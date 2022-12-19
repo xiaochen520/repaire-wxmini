abd32104399d1adc42f2d493826ff5fe4ea3437b
chenjd <chenjiadi@hzanchu.com>
2022-12-19 18:16:24 +0800

    init

---
+++ src/api/request.js
@@ -0,0 +1,16 @@
+export function request(option = {}) {
+    
+}
+
+export function get(url, data = {}) {
+    return new Promise((resolve, reject) => {
+
+    });
+}
+
+
+export function post(url, data = {}) {
+    return new Promise((resolve, reject) => {
+
+    });
+}
---
+++ src/app.config.js
@@ -0,0 +1,24 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index',
+    'pages/my/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页'
+      },
+      {
+        pagePath: 'pages/my/index',
+        text: '我的'
+      }
+    ]
+  }
+})
